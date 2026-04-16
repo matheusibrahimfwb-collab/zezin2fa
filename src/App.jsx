34ebf77aae6e9a583344190298ac14af29c4aa4e
matheus ibrahim <matheus.ibrahimfwb@gmail.com>
2026-04-15 21:16:33 -0300

    build deploy

--- src/App.jsx
+++ src/App.jsx
@@ -59,17 +59,16 @@ export default function App() {
     >
       <img
         src={background}
-  alt="background"
-  style={{
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-    zIndex: 0,
-  }}
-/>
+        alt=""
+        style={{
+          position: "absolute",
+          inset: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: 0,
+        }}
+      />
 
       <div
         style={{
